3a8882efee8f112784d5f3fb828c17233616b957
spulav <sgpulavarty@gmail.com>
2020-01-25 14:35:44 -0500

    changed structure of templates

--- config.py
+++
@@ -1,9 +0,0 @@
-import random
-
-PERMANENT_SESSION_LIFETIME = 300000
-TRAP_BAD_REQUEST_ERRORS = True
-SECRET_KEY = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
-                                          'abcdefghijklmnopqrstuvxyz' +
-                                          '0123456789'))
-                           for i in range(20) ])
-UPLOAD_FOLDER = '/uploaded/'
