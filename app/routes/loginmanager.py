b142ea5ced3c5e9c1b94403e4e1c5bcaacd91d8f
Svetha Pulavarty <sgpulavarty@gmail.com>
2023-04-08 13:07:19 -0400

    consolidating loginmanager files

--- app/routes/loginmanager.py
+++ app/routes/loginmanager.py
@@ -1,10 +1,15 @@
 from app import login_manager
-from app.services import loginmanager
+from app.dbmodels import User
+from flask import flash, redirect, url_for
 
 @login_manager.user_loader
 def load_user(user_id):
-    return loginmanager.load_user(user_id)
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    return loginmanager.unauthorized()
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('base_bp.home'))
