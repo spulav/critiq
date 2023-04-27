18f4a17ccfcd403595222da99f1607f16bead477
Svetha Pulavarty <sgpulavarty@gmail.com>
2023-04-27 02:57:22 -0400

    removing flask-bootstrap and just using bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_bootstrap import Bootstrap5
 from flask_migrate import Migrate
 import os
 
@@ -10,7 +9,6 @@ db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
 login_manager = LoginManager()
-bootstrap = Bootstrap5()
 
 def create_app():
     app = Flask(__name__)
@@ -29,7 +27,6 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-    bootstrap.init_app(app)
     mail.init_app(app)
 
     #import db models for initializing db
